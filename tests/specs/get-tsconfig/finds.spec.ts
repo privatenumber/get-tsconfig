36a2e69a19a44d6a38016ea7e19f5cca44265367
Hiroki Osame <hiroki.osame@gmail.com>
2022-06-28 12:00:02 -0400

    test: merged errors with finds

--- tests/specs/get-tsconfig/finds.spec.ts
+++ tests/specs/get-tsconfig/finds.spec.ts
@@ -1,10 +1,29 @@
 import path from 'path';
 import { testSuite, expect } from 'manten';
 import slash from 'slash';
+import { createFixture } from '../../utils/create-fixture';
 import { getTsconfig } from '#get-tsconfig'; // eslint-disable-line import/no-unresolved
 
 export default testSuite(({ describe }) => {
 	describe('find tsconfig', ({ test }) => {
+		describe('error cases', ({ test }) => {
+			test('tsconfig not found', () => {
+				const tsconfig = getTsconfig('/');
+
+				expect(tsconfig).toBe(null);
+			});
+
+			test('non json file', async () => {
+				const fixture = await createFixture({
+					'tsconfig.json': 'asdf',
+				});
+
+				expect(() => getTsconfig(fixture.path)).toThrow('Failed to parse JSON');
+
+				await fixture.cleanup();
+			});
+		});
+
 		test('from cwd', () => {
 			const tsconfig = getTsconfig();
 
