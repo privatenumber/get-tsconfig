12e7182ad78c6d9b1f87b788b6c66ef9e0a1e32b
JounQin <admin@1stg.me>
2022-07-05 04:47:11 +0000

    fix: support yarn pnp out of box

--- src/parse-tsconfig/resolve-extends.ts
+++ src/parse-tsconfig/resolve-extends.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs';
+import { createRequire } from 'module';
 import { findUp } from '../utils/find-up';
 
 const pathExists = (filePath: string) => fs.existsSync(filePath);
@@ -10,10 +11,8 @@ const safeJsonParse = (jsonString: string) => {
 	} catch {}
 };
 
-export function resolveExtends(
-	filePath: string,
-	directoryPath: string,
-) {
+// eslint-disable-next-line complexity
+export function resolveExtends(filePath: string, directoryPath: string) {
 	let currentFilePath = filePath;
 
 	if (currentFilePath === '..') {
@@ -24,10 +23,7 @@ export function resolveExtends(
 	if (currentFilePath.startsWith('.')) {
 		let tsconfigPath = path.resolve(directoryPath, currentFilePath);
 
-		if (
-			pathExists(tsconfigPath)
-			&& fs.statSync(tsconfigPath).isFile()
-		) {
+		if (pathExists(tsconfigPath) && fs.statSync(tsconfigPath).isFile()) {
 			return tsconfigPath;
 		}
 
@@ -37,6 +33,42 @@ export function resolveExtends(
 			if (pathExists(tsconfigPath)) {
 				return tsconfigPath;
 			}
+		}
+	} else if (process.versions.pnp) {
+		const require = createRequire(
+			// @ts-expect-error -- it will be transformed at build time
+			import.meta.url,
+		);
+		// @see https://yarnpkg.com/advanced/pnpapi/#requirepnpapi
+		// eslint-disable-next-line import/no-unresolved -- builtin module from yarn PnP
+		const pnpapi = require('pnpapi');
+		const [first, second] = filePath.split('/');
+		const pkg = first.startsWith('@') ? `${first}/${second}` : first;
+		if (pkg === filePath) {
+			try {
+				const packageJsonpath = pnpapi.resolveRequest(
+					`${pkg}/package.json`,
+					directoryPath,
+				);
+				const packageJson = safeJsonParse(
+					fs.readFileSync(packageJsonpath, 'utf8'),
+				);
+				let packagePath = path.join(packageJsonpath, '..');
+				if (packageJson && 'tsconfig' in packageJson) {
+					packagePath = path.join(packagePath, packageJson.tsconfig);
+				} else {
+					packagePath = path.join(packagePath, 'tsconfig.json');
+				}
+				if (pathExists(packagePath)) {
+					return packagePath;
+				}
+			} catch {}
+		} else {
+			try {
+				return pnpapi.resolveRequest(filePath, directoryPath, {
+					extensions: ['.json'],
+				});
+			} catch {}
 		}
 	} else {
 		let packagePath = findUp(
@@ -49,7 +81,9 @@ export function resolveExtends(
 				const packageJsonpath = path.join(packagePath, 'package.json');
 
 				if (pathExists(packageJsonpath)) {
-					const packageJson = safeJsonParse(fs.readFileSync(packageJsonpath, 'utf8'));
+					const packageJson = safeJsonParse(
+						fs.readFileSync(packageJsonpath, 'utf8'),
+					);
 
 					if (packageJson && 'tsconfig' in packageJson) {
 						packagePath = path.join(packagePath, packageJson.tsconfig);
