8d81d8a7dbab7c01bf61474141b2a26db66753fa
Hiroki Osame <hiroki.osame@gmail.com>
2022-08-08 01:28:41 -0400

    test: separate get-tsconfig from parse-tsconfig tests

--- tests/specs/parse-tsconfig/index.ts
+++ tests/specs/parse-tsconfig/index.ts
@@ -1,7 +1,6 @@
 import { testSuite } from 'manten';
 
 export default testSuite(({ runTestSuite }) => {
-	runTestSuite(import('./finds.spec.js'));
 	runTestSuite(import('./parses.spec.js'));
 	runTestSuite(import('./extends/index.js'));
 });
