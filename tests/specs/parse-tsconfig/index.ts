e09b84d16f24e001f8c89e472b14977775a09def
Hiroki Osame <hiroki.osame@gmail.com>
2023-05-29 14:53:29 +0900

    wip

--- tests/specs/parse-tsconfig/index.ts
+++ tests/specs/parse-tsconfig/index.ts
@@ -1,6 +1,6 @@
 import { testSuite } from 'manten';
 
 export default testSuite(({ runTestSuite }) => {
-	// runTestSuite(import('./parses.spec.js'));
+	runTestSuite(import('./parses.spec.js'));
 	runTestSuite(import('./extends/index.js'));
 });
