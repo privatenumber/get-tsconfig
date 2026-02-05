c0e831bc92eda0e7265ecefa4865cf58539e6ae1
Hiroki Osame <hiroki.osame@gmail.com>
2026-02-06 00:27:20 +0900

    fix(parse-tsconfig): normalize lib array values to lowercase

--- src/parse-tsconfig/index.ts
+++ src/parse-tsconfig/index.ts
@@ -433,6 +433,12 @@ const normalizeCompilerOptions = (
 		}
 	}
 
+	if (compilerOptions.lib) {
+		compilerOptions.lib = compilerOptions.lib.map(
+			library => library.toLowerCase() as TsConfigJson.CompilerOptions.Lib,
+		);
+	}
+
 	if (compilerOptions.esModuleInterop) {
 		compilerOptions.allowSyntheticDefaultImports ??= true;
 	}
